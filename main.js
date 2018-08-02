3e5fbcd4f76b2a76cab97789f815c1a2649afdcb
Matt Outen <mattoutenphysed@gmail.com>
2018-08-02 11:56:41 -0500

    basic node app:complete

---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require('./greeting.js');
+const goodbye = require('./goodbye.js')
+//'Hello'
+greetings.greetingEnglish();
+
+//'Konichiwa'
+greetings.greetingJapanese();
+
+//'Hola'
+greetings.greetingSpanish();
+
+//Halló
+greetings.greetingIcelandic();
+
+//Goodbye
+goodbye.goodbyeEnglish();
